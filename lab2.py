8af27858506f4f24bce5890ca06a8a24433340a8
jellyshroom <kihyvr@gmail.com>
2025-01-29 14:15:41 -0800

    lab 2 complete

--- lab2.py
+++ lab2.py
@@ -5,15 +5,16 @@ jlsheng@usc.edu
 Lab 2
 '''
 
-name = input("Enter your name: ")
-if len(name) == 0:
-    print("You entered nothing. Exiting program.")
-    exit()
-else:
-    # counter
-    vowel_count = 0
-    for letter in name:
-        if letter.lower() in ['a', 'e', 'i', 'o', 'u', 'y']:
-            vowel_count += 1
+while True:
+    name = input("Enter your name (enter nothing to exit): ")
+    if len(name) == 0:
+        print("You entered nothing. Exiting program.")
+        break
+    else:
+        # counter
+        vowel_count = 0
+        for letter in name:
+            if letter.lower() in ['a', 'e', 'i', 'o', 'u', 'y']:
+                vowel_count += 1
 
-    print(f"Number of vowels: {vowel_count}")
+        print(f"Number of vowels: {vowel_count}")
